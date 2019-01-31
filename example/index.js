9b39fa711c9d03a9c33377f630f0fe8bf1385762
Yuanying Du <janiec@YuanyingdeMacBook-Pro.local>
2019-01-31 16:27:16 +0800

    update

---
+++ example/index.js
@@ -0,0 +1,158 @@
+const NodeSwift = require('./../index');
+const fs = require('fs');
+
+function printObj(obj) {
+  console.log(JSON.stringify(obj))
+}
+
+function printErr(obj) {
+  console.error(JSON.stringify(obj))
+}
+
+async function example() {
+  const client = new NodeSwift({
+    host: "127.0.0.1",
+    port: 12345
+  });
+  
+  /**
+   * Login
+   */
+  await client.login({
+    account: 'test',
+    user:'tester',
+    password:'testing'
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+  
+  /**
+   * Get Container list
+   */
+  await client.getContainers().then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Create a new Container
+   */
+  await client.newContainer('test').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Create a new Container
+   * for option items ref: https://docs.openstack.org/swift/latest/overview_acl.html#container-acls
+   */
+  await client.newContainer('test1', {
+    headers: {
+      'X-Container-Read': '.r:*'
+    }
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Delete a Container
+   */
+  await client.delContainer('test1').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Create a new Object
+   */
+  await client.newObject('test', 'object1', 'hello world', {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  const stream = fs.createReadStream('./example/WechatIMG127.jpeg');
+  /**
+   * Create a new Object
+   */
+  await client.newObject('test', 'object2', stream, {
+    headers: {
+      'Content-Type': 'image/jpeg'
+    }
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Update a Object
+   */
+  await client.updateObject('test', 'object1', 'hello world two', {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Create a new Object
+   */
+  await client.getObjects('test').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Delete a Object
+   */
+  await client.delObject('test', 'object1').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  /**
+   * Create a new Object
+   */
+  await client.getObjects('test').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+
+  await client.newContainer('test1', {
+    headers: {
+      'X-Container-Read': '.r:*'
+    }
+  }).then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+  /**
+   * Create a new Object
+   */
+  await client.copyObject('test', 'object2', 'test1', 'object1').then((d) => {
+    printObj(d);
+  }).catch((e) => {
+    printErr(e);
+  })
+}
+
+example();
